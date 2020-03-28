2128f2f45aa406ec80a4b3c07e755e2b379d9180
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-28 00:34:53 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+def greeting_a_programmer (name = "neighbor",language = "Python")
+  puts "Hello #{name}. We heard you are a great #{language} programmer."
+end
+greeting_a_programmer("Julian", "Brainfuck")
---
+++ learning-if-end.rb
@@ -0,0 +1,19 @@
+# run_code_inside = false
+# puts "Code before if..end"
+# if run_code_inside
+#  puts "code inside"
+# end
+# puts "Code after if...end" 
+
+# chance_of_rain = 0.6
+#if chance_of_rain <= 0.5
+ # puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella!"
+#else 
+#  puts "stay home and read Hegel."
+#end
+
+puts "You know what year it is??"
+this_year = Time.now.year
+puts "Hey, it's 2019!" if this_year == 2020
